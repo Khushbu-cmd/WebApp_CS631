e3728e75d2ab6b4c40cdc4e7994c10fb1c13e448
Kaushal Patel <kaushalpatel@Kaushals-iMac.fios-router.home>
2022-04-25 19:53:06 -0400

    app using flask

--- main.py
+++ main.py
@@ -2,15 +2,18 @@
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+from flask import Flask
+
+app = Flask(__name__)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
+@app.route('/')
+def hello_world():
+    return 'Hello World'
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+
+    app.run()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/

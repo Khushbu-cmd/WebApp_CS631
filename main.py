758ed0cb9f4e478cec697b3c4cce77b92cda76b3
Kaushal Patel <kaushalpatel@Kaushals-iMac.fios-router.home>
2022-04-26 22:21:27 -0400

    w

--- main.py
+++ main.py
@@ -5,20 +5,23 @@
 
 import psycopg2
 
-from flask import Flask
+from flask import Flask, render_template
+
 
 conn = psycopg2.connect("dbname=company user=flaskuser password=password")
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello_world():
-    return 'Hello World'
+def index():
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    return render_template('login.html')
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-
     app.run()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
